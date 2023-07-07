6e65844aff19988b09acc04c9782fa18898e937f
MagneticMojo <forsberg.bjorn.a@gmail.com>
2023-07-07 04:56:12 +0200

    Comments added for: GameController, RockPaperScissorsExceptionHandler, Move, GameException, PlayerException

--- src/main/java/com/magneticmojo/rpsapi/api/exceptions/playerexception/PlayerException.java
+++ src/main/java/com/magneticmojo/rpsapi/api/exceptions/playerexception/PlayerException.java
@@ -1,5 +1,10 @@
 package com.magneticmojo.rpsapi.api.exceptions.playerexception;
 
+/**
+ * PlayerException is a custom runtime exception for the Rock-Paper-Scissors game application.
+ * It is used to signify an error condition related to a player's actions or status within a game.
+ * This class serves as a base exception from which other more specific player-related exceptions are derived.
+ */
 public class PlayerException extends RuntimeException {
     public PlayerException(String errorMessage) {
         super(errorMessage);
