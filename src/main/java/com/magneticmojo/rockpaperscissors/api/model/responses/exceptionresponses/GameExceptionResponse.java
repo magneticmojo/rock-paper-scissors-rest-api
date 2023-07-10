a9a5e451cbaa213ba60f7b0bc9cc94cf04e580eb
MagneticMojo <forsberg.bjorn.a@gmail.com>
2023-07-10 21:11:25 +0200

    Controller, EXHand1, EXHAND2, GameExRes, ReqValErrRes,

--- src/main/java/com/magneticmojo/rockpaperscissors/api/model/responses/exceptionresponses/GameExceptionResponse.java
+++ src/main/java/com/magneticmojo/rockpaperscissors/api/model/responses/exceptionresponses/GameExceptionResponse.java
@@ -2,6 +2,7 @@ package com.magneticmojo.rockpaperscissors.api.model.responses.exceptionresponse
 
 /**
  * Represents a generic game exception error response.
+ * The class is used by the RockPaperScissorsGameExceptionHandler.
  */
 public record GameExceptionResponse(String errorCode, String errorMessage) {
 }
--- src/main/java/com/magneticmojo/rockpaperscissors/api/model/responses/exceptionresponses/RequestValidationErrorResponse.java
+++ src/main/java/com/magneticmojo/rockpaperscissors/api/model/responses/exceptionresponses/RequestValidationErrorResponse.java
@@ -3,7 +3,7 @@ package com.magneticmojo.rockpaperscissors.api.model.responses.exceptionresponse
 import java.util.List;
 
 /**
- * Represents a response for validation errors.
+ * Represents a response for validation errors. The class is used by the RequestValidationExceptionHandler.
  * The response includes the HTTP status code and a list of error messages.
  */
 public record RequestValidationErrorResponse(int HttpStatusCode, List<String> errors) {
